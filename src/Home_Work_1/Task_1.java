606f43527754030e5cb3f142ea05223f1e8e27c7
jekazm4211 <jekazm@mail.ru>
2021-03-13 11:53:04 +0300

    3 tasks ready

--- src/Home_Work_1/Task_1.java
+++ src/Home_Work_1/Task_1.java
@@ -1,15 +1,14 @@
 package Home_Work_1;
 
 public class Task_1 {
-    public static void main (String[]args){
+    public static void main(String[] args) {
         int[] mass = {5, 6, 3, 1, 4, 2};
         int n = 3;
         int summa = 0;
         for (int i = 0; i < mass.length; i++) {
-            if(mass[i] == n){
+            if (mass[i] == n) {
                 continue;
-            }
-            else {
+            } else {
                 summa = summa + mass[i];
             }
         }
--- src/Home_Work_1/Task_2.java
+++ src/Home_Work_1/Task_2.java
@@ -1,11 +1,22 @@
 package Home_Work_1;
 
 public class Task_2 {
-    public static void main (String[]args) {
-
-
-
-
-
+    public static void main(String[] args) {
+        int n = 7;
+        int a = 0;
+        int b = 1;
+        int next;
+        int summa = 0;
+        for (int i = 2; i < n; i++) {
+            next = a + b;
+            a = b;
+            b = next;
+            if (next % 2 == 0) {
+                summa += next;
+            }
+        }
+        System.out.print("Сумма всех четных чисел до n равна: " + summa);
     }
 }
+
+
