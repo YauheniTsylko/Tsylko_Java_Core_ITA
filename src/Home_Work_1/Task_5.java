caa1317104d5468473195de32455e921dfb743b4
jekazm4211 <jekazm@mail.ru>
2021-03-13 17:22:51 +0300

    ready

--- src/Home_Work_1/Task_5.java
+++ src/Home_Work_1/Task_5.java
@@ -4,21 +4,21 @@ package Home_Work_1;
 
 public class Task_5 {
     public static void main(String args[]){
-        int[][] matrix ={{4, 0, 0, 0, 0},
+        int[][] matrix ={{4, 0, 5, 0, 0},
                          {1, 0, 0, 0, 0},
                          {9, 3, 1, 0, 0},
                          {1, 0, 8, 0, 0},
                          {1, 6, 1, 0, 6}};
-        int p = 0;
+        boolean p = true;
         for(int i=0; i < matrix.length; i++){
             for(int j=i+1; j < matrix[i].length; j++) {
                 if(matrix[i][j]!=0) {
-                    p = 1;
+                    p = false;
                     break;
                 }
             }
         }
-        if(p==0){
+        if(p){
              System.out.println("Матрица является нижней треугольной");
         } else{
             System.out.println("Матрица не является нижней треугольной");
